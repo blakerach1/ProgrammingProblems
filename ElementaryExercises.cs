66a5de420b44cda3862eaf13c864580cca78ceee
tabrbl-admin <tabrbl@rwwa.com.au>
2024-10-03 09:08:14 +0800

    SIMPLE_PROGRAMMING_PROBLEMS: Elementary exercises 1 through 6 - solved in C#

---
+++ ElementaryExercises.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingProblems
+{
+    public class ElementaryExercises
+    {
+        public static void SumNumbers(int inputNumber)
+        {
+            int sumNumber = 0;
+            for (int i = inputNumber; i >= 0; i--)
+            {
+                sumNumber += i;
+            }
+            Console.WriteLine($"The sum from 1 to {inputNumber} is {sumNumber}");
+        }
+
+        public static void ProductOfNumbers(int inputNumber)
+        {
+            int productOfNumber = 1;
+            for (int i = inputNumber; i > 0; i--)
+            {
+                productOfNumber *= i;
+            }
+            Console.WriteLine($"The product of numbers from 1 to {inputNumber} is {productOfNumber}");
+        }
+
+        public static void SumMultiples(int enteredNumber, int multipleOne, int multipleTwo)
+        {
+            int sumNumber = 0;
+            for (int i = enteredNumber; i >= 0; i--)
+            {
+                if (i % multipleOne == 0 || i % multipleTwo == 0)
+                {
+                    sumNumber += i;
+                }
+                
+            }
+            Console.WriteLine($"The summed of numbers between 0 and {enteredNumber} that are multiples of {multipleOne} or {multipleTwo} is {sumNumber}");
+        }
+
+        public static void Greeting(string userName)
+        {
+            if (userName == "alice" || userName == "bob")
+            {
+                Console.WriteLine($"Hello, {userName}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ProgrammingProblems;
+using System.ComponentModel;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //Console.Write("Enter your name: ");
+        //var userName = Console.ReadLine()?.ToLower();
+        //ElementaryExercises.Greeting(userName);
+
+
+        //Console.Write("Enter a number: ");
+        //var inputNumber = Convert.ToInt32(Console.ReadLine());
+        //ElementaryExercises.SumNumbers(inputNumber);
+
+
+        //Console.WriteLine(
+        //    "Enter a number and two multiples. \n" +
+        //    "Numbers between 0 and your input number will be summed only if \n" + 
+        //    "they are multiples of the two values you've entered as multiples. \n");
+        //Console.Write("Enter a number: ");
+        //var enteredNumber = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter your first multiple: ");
+        //var multipleOne = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter your second multiple: ");
+        //var multipleTwo = Convert.ToInt32(Console.ReadLine());
+        //ElementaryExercises.SumMultiples(enteredNumber, multipleOne, multipleTwo);
+
+
+        Console.Write("Enter a number: ");
+        var inputNumber = Convert.ToInt32(Console.ReadLine());
+        string userChoice;
+
+        do
+        {
+            Console.WriteLine(
+                "Would you like to compute the sum (a) or the product (b) of the numbers from 1 to your input number? \n" +
+                "Please type a or b: ");
+            userChoice = Console.ReadLine()?.ToLower();
+
+            if (userChoice == "a")
+            {
+                ElementaryExercises.SumNumbers(inputNumber);
+            }
+            else if (userChoice == "b")
+            {
+                ElementaryExercises.ProductOfNumbers(inputNumber);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please type a or b.");
+
+            }
+        } while (userChoice != "a" && userChoice != "b");
+
+    }
+}
